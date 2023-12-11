c691b8c3ef74bcfd7b18d3da077125b7a2dfa22b
pbaldisa <pabasa3@gmail.com>
2023-12-11 12:56:25 +0100

    CNF MNIST

---
+++ application_examples/continuous_normalising_flows/modelling/utils.py
@@ -0,0 +1,15 @@
+from .layers.cnf import CNF
+def count_total_time(model):
+
+    class Accumulator(object):
+
+        def __init__(self):
+            self.total_time = 0
+
+        def __call__(self, module):
+            if isinstance(module, CNF):
+                self.total_time = self.total_time + module.sqrt_end_time * module.sqrt_end_time
+
+    accumulator = Accumulator()
+    model.apply(accumulator)
+    return accumulator.total_time
